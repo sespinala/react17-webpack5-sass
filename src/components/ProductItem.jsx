5c234f151c19e8d8eac24e04f60232fffff1cf51
Sara Espinal <saraespinal1@gmail.com>
2023-05-12 16:46:37 -0500

    add custom hook

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -3,7 +3,7 @@ import '@styles/ProductItem.scss';
 
 import addToCart from '@icons/bt_add_to_cart.svg';
 
-const ProductItem = () => {
+const ProductItem = ({ product }) => {
 	const [cart, setCart] = useState([]);
 
 	const handleClick = () => {
@@ -12,11 +12,11 @@ const ProductItem = () => {
 
 	return (
 		<div className="ProductItem">
-			<img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" />
+			<img src={product.images[0]} alt={product.title} />
 			<div className="product-info">
 				<div>
-					<p>$120,00</p>
-					<p>Bike</p>
+					<p>${product.price}</p>
+					<p>{product.title}</p>
 				</div>
 				<figure onClick={handleClick}>
 					<img src={addToCart} alt="" />
--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,7 @@ module.exports = {
             '@styles': path.resolve(__dirname, 'src/styles/'),
             '@icons': path.resolve(__dirname, 'src/assets/icons/'),
             '@logos': path.resolve(__dirname, 'src/assets/logos/'),
+            '@hooks': path.resolve(__dirname, 'src/hooks/'),
         }
     },
     module: {
