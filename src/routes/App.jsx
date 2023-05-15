aaaf1e30fcb4c519b72b753d366354f1ccfe6a66
Sara Espinal <saraespinal1@gmail.com>
2023-05-15 17:34:11 -0500

    updates to actions

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -19,8 +19,7 @@ const App = () => {
 	const initialState = useInitialState();
 	return (
 		<AppContext.Provider value={initialState}>
-			
-			<BrowserRouter>
+			<BrowserRouter basename='/react-shop'>
 				<Layout>
 					<Routes>
 						<Route exact path="/" element={<Home />} />
